eb839e096e0e583ee9abf344c94a4c91e98aa5a0
nestorpineda@gmail.com <>
2014-05-08 23:04:19 +0000

    Formated for Eclipse

--- src/main/java/hello/SayHello.java
+++ src/main/java/hello/SayHello.java
@@ -1,14 +1,12 @@
 package hello;
-  
- /**
+
+/**
  * Hello world!
  *
  */
 
-public class SayHello 
-{
-    public String sayHello(String name)
-    {
+public class SayHello {
+    public String sayHello(String name) {
         return "Hello, " + name;
     }
 }
